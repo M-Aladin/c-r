16e48d386f4af1e9f2537b9ef2619c6215471c43
Alfonso Reyes <alfonso.r.reyes@gmail.com>
2017-08-04 11:23:24 -0500

    document 1D convolution Cpp code

--- src/convolve.cpp
+++ src/convolve.cpp
@@ -1,12 +1,13 @@
 // called by rcpp-sourceCpp_convolveCpp.Rmd
+
 #include <Rcpp.h> 
 using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericVector convolveCpp(NumericVector a, NumericVector b) {
-  int na = a.size(), nb = b.size();
-  int nab = na + nb - 1;
-  NumericVector xab(nab);
+  int na = a.size(), nb = b.size();       // size of the vectors
+  int nab = na + nb - 1;                  // size of the new vector
+  NumericVector xab(nab);                 // output vector
   
   for (int i = 0; i < na; i++)
     for (int j = 0; j < nb; j++)
